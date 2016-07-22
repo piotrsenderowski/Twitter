bf973ab835d6f2680fa435a28c75e454b507a8d9
piotrsenderowski <senderowski@gmail.com>
2016-07-22 13:39:04 +0200

    database added

--- src/twitter.sql
+++ src/twitter.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 20 Lip 2016, 22:46
+-- Czas wygenerowania: 22 Lip 2016, 11:37
 -- Wersja serwera: 5.5.49-0ubuntu0.14.04.1
 -- Wersja PHP: 5.5.9-1ubuntu4.17
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `Comment` (
   PRIMARY KEY (`id`),
   KEY `postId` (`postId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- Zrzut danych tabeli `Comment`
@@ -49,7 +49,11 @@ INSERT INTO `Comment` (`id`, `userId`, `postId`, `creationDate`, `commentText`) 
 (5, 1, 4, '2016-07-20 22:07:21', 'Dodam kolejny komentarz i zobaczę co się stanie.'),
 (6, 1, 4, '2016-07-20 22:07:32', 'Jeszcze jeden dla przetestowania.'),
 (7, 2, 5, '2016-07-20 22:07:19', 'To jest pierwszy komentarz do pierwszego tweeta Pawła'),
-(8, 3, 6, '2016-07-20 22:07:56', 'To jest pierwszy comment pierwszego tweeta Jana.');
+(8, 3, 6, '2016-07-20 22:07:56', 'To jest pierwszy comment pierwszego tweeta Jana.'),
+(9, 1, 4, '2016-07-21 07:07:54', 'Dodaję komentarz.'),
+(12, 1, 5, '2016-07-21 09:07:43', 'To jest pierwszy komentarz do pierwsze tweeta Pawła'),
+(13, 1, 6, '2016-07-21 13:07:38', 'Hello comment'),
+(14, 1, 13, '2016-07-21 14:07:20', 'asdsasdasd');
 
 -- --------------------------------------------------------
 
@@ -63,10 +67,42 @@ CREATE TABLE IF NOT EXISTS `Message` (
   `receiverId` int(11) DEFAULT NULL,
   `messageText` text NOT NULL,
   `messageStatus` tinyint(4) DEFAULT '0',
+  `sendDate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `senderId` (`senderId`),
   KEY `receiverId` (`receiverId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+
+--
+-- Zrzut danych tabeli `Message`
+--
+
+INSERT INTO `Message` (`id`, `senderId`, `receiverId`, `messageText`, `messageStatus`, `sendDate`) VALUES
+(1, 1, 2, 'From Piotr to Paweł.', 0, '2016-07-21 16:07:26'),
+(6, 3, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 16:07:08'),
+(7, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 16:07:12'),
+(8, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 16:07:12'),
+(9, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 16:07:27'),
+(10, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:11'),
+(11, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:16'),
+(12, 2, 1, '', 1, '2016-07-21 17:07:53'),
+(13, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:23'),
+(14, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:41'),
+(15, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:21'),
+(16, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:59'),
+(17, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:27'),
+(18, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:37'),
+(19, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:37'),
+(20, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:38'),
+(21, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:38'),
+(22, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:38'),
+(23, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:38'),
+(24, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:38'),
+(25, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:48'),
+(26, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:41'),
+(27, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:14'),
+(28, 1, 2, 'Przykładowa treść wiadomości', 0, '2016-07-21 17:07:15'),
+(29, 1, 2, 'Przykładowa treść wiadomości', 1, '2016-07-21 17:07:21');
 
 -- --------------------------------------------------------
 
@@ -80,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `Tweet` (
   `tweetText` varchar(140) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 --
 -- Zrzut danych tabeli `Tweet`
@@ -92,7 +128,14 @@ INSERT INTO `Tweet` (`id`, `userId`, `tweetText`) VALUES
 (3, 1, 'To treść trzeciego Tweeta Piotra.'),
 (4, 1, 'To treść czwartego Tweeta Piotra.'),
 (5, 2, 'To jest pierwszy Tweet Pawła.'),
-(6, 3, 'To jest pierwszy Tweet Jana');
+(6, 3, 'To jest pierwszy Tweet Jana'),
+(7, 1, 'To treść piątego Tweeta Piotra.'),
+(8, 1, 'To treść piątego Tweeta Piotra.'),
+(9, 1, 'To treść piątego Tweeta Piotra.'),
+(10, 1, 'To jest treść szóstego Tweeta Piotra.'),
+(11, 1, 'To jest treść szóstego Tweeta Piotra.'),
+(12, 1, 'To jest treść siódmego Tweeta Piotra.'),
+(13, 1, 'To jest treść ósmego Tweeta Piotra.');
 
 -- --------------------------------------------------------
 
@@ -115,8 +158,8 @@ CREATE TABLE IF NOT EXISTS `User` (
 --
 
 INSERT INTO `User` (`id`, `email`, `password`, `fullName`, `active`) VALUES
-(1, 'piotr@test.pl', '$2y$10$pWmA/iMvuTHgKVfskwDjQ.FCLVMqQCDtxuQysW6kIu1rdY0wsuHg.', 'PiotrTestowy', 1),
-(2, 'pawel@test.pl', '$2y$10$BesHHnoum8T4.ONold.HiuNUb5In6dg/fHr6lNIyOvNixrFHvHKqa', 'PawelTestowy', 1),
+(1, 'piotr@test.pl', '', 'PiotrTestowyNewName', 1),
+(2, 'pawel@test.pl', '$2y$10$ZMEbauhevIIuQ/WtN91Ui.MqqYkeNkSs8o5xLektyHwdej7neCgRa', 'PawelTestowy', 1),
 (3, 'jan@test.pl', '$2y$10$4oYwGg6FM7l/eWPcuBIq/O4J9WdIA0GKQ.EJzICzMgR2Cp.VjhjNu', 'JanTestowy', 1);
 
 --
